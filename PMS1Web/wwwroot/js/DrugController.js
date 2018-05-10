73e7c4c2feefde6a037a91a26ba67c95f4fa0b09
Tareq <Tareq@DESKTOP-BV0JIPJ>
2018-05-10 21:02:00 +0600

    Api call to Craate drug added

--- PMS1Web/wwwroot/js/DrugController.js
+++ PMS1Web/wwwroot/js/DrugController.js
@@ -6,26 +6,28 @@
 var drugController = new Vue({
     el: '#DrugController',
     data: {
-        Model: {
-            DrugName: null,
-            DrugPrice: null
+        Drug: {
+            Name: null,
+            Price: null
         }
     },
     methods: {
         SaveDrug: function ()
         {
-            $.post('http://192.168.10.139:7301/api/drug/create',
-                drugController.Model)
-                .then(
-                    function success(a, b, c, d)
-                    {
-
-                    },
-                    function error(a,b,c,d)
-                    {
-
-                    }
-            );
+            $.ajax({
+                url: 'https://localhost:44336/api/drug/create',
+                method: 'POST',
+                data: JSON.stringify(drugController.Drug),
+                contentType: 'application/json'
+            })
+                .then(function success(data, textStatus, jqXHR)
+                {
+                    console.log('Status: ' + textStatus + ' ||  Response Data :' + data);
+                },
+                function error(jqXHR, textStatus, errorThrown)
+                {
+                    console.warn('Status: ' + textStatus + ' ||  Response Data :' + errorThrown);
+                });
         }
     }
 });
